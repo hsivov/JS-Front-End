8b64a38cfc1d37c816451d7624b14cbefb5d5fb0
hsivov <hsivov@students.softuni.bg>
2023-03-08 12:28:32 +0200

    Objects and Classes

---
+++ JS Front End/03.Objects and Classes/Lab/01.person-info.js
@@ -0,0 +1,14 @@
+function personInfo(firstName, lastName, age) {
+
+    return {
+        firstName,
+        lastName,
+        age,
+    };
+}
+
+console.log(
+    personInfo("Peter",
+        "Pan",
+        "20")
+);
