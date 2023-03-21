3cabcfddc16b1f385e5cd278d7905cfa114415cb
hsivov <hsivov@students.softuni.bg>
2023-03-21 17:04:18 +0200

    DOM and Events more exercises

--- JS Front End/04.DOM and Events/More/12. Encode-and-Decode-Messages/app.js
+++ JS Front End/04.DOM and Events/More/12. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeTextArea, decodeTextArea] = document.getElementsByTagName('textarea');
+    const [encodeBtn, decodeBtn] = document.getElementsByTagName('button');
+
+    encodeBtn.addEventListener('click', encodeHandler);
+    decodeBtn.addEventListener('click', decodeHandler);
+
+    function encodeHandler() {
+        let textToEncode = encodeTextArea.value;
+        let encoded = [];
+        for (const symbol of textToEncode.split('')) {
+            let ascii = symbol.charCodeAt(0);
+            ascii += 1;
+            encoded.push(String.fromCharCode(ascii));
+        }
+        encodeTextArea.value = '';
+        decodeTextArea.value = encoded.join('');
+    }
+
+    function decodeHandler() {
+        let textToDecode = decodeTextArea.value;
+        let decoded = [];
+        for (const symbol of textToDecode.split('')) {
+            let ascii = symbol.charCodeAt(0);
+            ascii -= 1;
+            decoded.push(String.fromCharCode(ascii));
+        }
+        decodeTextArea.value = decoded.join('');
+    }
 }
--- JS Front End/04.DOM and Events/More/13. Distance-Converter/distanceConverter.js
+++ JS Front End/04.DOM and Events/More/13. Distance-Converter/distanceConverter.js
@@ -1,3 +1,71 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const inputDistance = document.getElementById('inputDistance');
+    const convertBtn = document.getElementById('convert');
+    const selectInputUnitsEl = document.getElementById('inputUnits');
+    const selectOutputUnitsEl = document.getElementById('outputUnits');
+    const outputDistance = document.getElementById('outputDistance');
+
+    let inputUnits = selectInputUnitsEl.value;
+    let outputUnits = selectOutputUnitsEl.value;
+
+    convertBtn.addEventListener('click', () => {
+        let meters = convertToMeters(inputDistance.value, inputUnits);
+        outputDistance.value = convertFromMeters(meters, outputUnits);
+    });
+
+    selectInputUnitsEl.addEventListener('change', () => {
+        inputUnits = selectInputUnitsEl.value;
+    });
+
+    selectOutputUnitsEl.addEventListener('change', () => {
+       outputUnits = selectOutputUnitsEl.value;
+    });
+
+    function convertToMeters(distance, convertFrom) {
+        const km = (d) => d * 1000;
+        const m = (d) => d * 1;
+        const cm = (d) => d * 0.01;
+        const mm = (d) => d * 0.001;
+        const mi = (d) => d * 1609.34;
+        const yrd = (d) => d * 0.9144;
+        const ft = (d) => d * 0.3048;
+        const inches = (d) => d * 0.0254;
+
+        let conversionMap = {
+            km,
+            m,
+            cm,
+            mm,
+            mi,
+            yrd,
+            ft,
+            in: inches
+        }
+
+        return conversionMap[convertFrom](distance);
+    }
+
+    function convertFromMeters(distance, convertTo) {
+        const km = (d) => d / 1000;
+        const m = (d) => d / 1;
+        const cm = (d) => d / 0.01;
+        const mm = (d) => d / 0.001;
+        const mi = (d) => d / 1609.34;
+        const yrd = (d) => d / 0.9144;
+        const ft = (d) => d / 0.3048;
+        const inches = (d) => d / 0.0254;
+
+        let conversionMap = {
+            km,
+            m,
+            cm,
+            mm,
+            mi,
+            yrd,
+            ft,
+            in: inches
+        }
+
+        return conversionMap[convertTo](distance);
+    }
 }
