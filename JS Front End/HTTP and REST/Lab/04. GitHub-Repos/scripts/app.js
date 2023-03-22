49e90b124ff51bce9334aaa4949776b3c7c5bfea
hsivov <hsivov@students.softuni.bg>
2023-03-22 19:34:20 +0200

    HTTP and REST lab

--- JS Front End/HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
+++ JS Front End/HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,13 @@
 function loadRepos() {
-   console.log("TODO...");
+   const URL = 'https://api.github.com/users/testnakov/repos';
+   const result = document.getElementById('res');
+
+   fetch(URL)
+       .then((res) => res.text())
+       .then((text) => {
+          result.textContent = text;
+       })
+       .catch((err) => {
+          alert(err);
+       });
 }
--- JS Front End/HTTP and REST/Lab/05. Github-Repos-by-Username/scripts/app.js
+++ JS Front End/HTTP and REST/Lab/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,25 @@
 function loadRepos() {
-	console.log("TODO...");
+    const input = document.getElementById('username');
+    const BASE_URL = 'https://api.github.com/users/';
+    const repoList = document.getElementById('repos');
+
+    repoList.innerHTML = '';
+
+    fetch(`${BASE_URL}${input.value}/repos`)
+        .then((res) => res.json())
+        .then((repos) => {
+            repos
+                .forEach((repo) => {
+                    const {full_name, html_url} = repo;
+                    const li = document.createElement('li');
+                    const anchor = document.createElement('a');
+                    anchor.setAttribute('href', html_url);
+                    anchor.textContent = full_name;
+                    li.appendChild(anchor);
+                    repoList.appendChild(li);
+                });
+        })
+        .catch((error) =>{
+            alert(error);
+        });
 }
