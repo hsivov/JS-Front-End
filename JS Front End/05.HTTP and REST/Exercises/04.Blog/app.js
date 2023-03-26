d972f906ca2dc2fcee68b5afca92480665067f0e
hsivov <hsivov@students.softuni.bg>
2023-03-27 01:00:47 +0300

    HTTP and REST exercises

--- JS Front End/05.HTTP and REST/Exercises/04.Blog/app.js
+++ JS Front End/05.HTTP and REST/Exercises/04.Blog/app.js
@@ -1,5 +1,65 @@
 function attachEvents() {
-    // TODO:
+    const loadPostsBtn = document.getElementById('btnLoadPosts');
+    const selectElement = document.getElementById('posts');
+    const viewPostBtn = document.getElementById('btnViewPost');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const commentsList = document.getElementById('post-comments');
+    let posts = [];
+
+    loadPostsBtn.addEventListener('click', loadPostsHandler);
+    viewPostBtn.addEventListener('click', viewPostHandler);
+    selectElement.addEventListener('change', selectOptionHandler);
+
+    async function loadPostsHandler() {
+        const URL = 'http://localhost:3030/jsonstore/blog/posts';
+
+        const response = await fetch(URL);
+        const responseData = await response.json();
+
+        Object.values(responseData).forEach((value) => {
+            const {id, title} = value;
+            const option = document.createElement('option');
+            option.value = id.toString();
+            option.textContent = title.toString();
+            selectElement.appendChild(option);
+            posts.push(value);
+        });
+        viewPostBtn.id = selectElement.value;
+    }
+
+    function viewPostHandler() {
+        const URL = 'http://localhost:3030/jsonstore/blog/comments';
+        const topicId = this.id;
+
+        const post = posts.find((post) => post.id === topicId);
+        const {title, body} = post;
+        postTitle.textContent = title;
+        postBody.textContent = body;
+
+
+        fetch(URL)
+            .then((commentsRes) => commentsRes.json())
+            .then((commentsResData) => {
+                commentsList.innerHTML = '';
+
+                Object.values(commentsResData)
+                    .filter((value) => value.postId === topicId)
+                    .forEach((value) => {
+                        const {text} = value;
+                        const comment = document.createElement('li');
+                        comment.textContent = text.toString();
+                        commentsList.appendChild(comment);
+                    });
+            })
+            .catch((err) => {
+                alert(err);
+            })
+    }
+
+    function selectOptionHandler() {
+        viewPostBtn.id = selectElement.value;
+    }
 }
 
 attachEvents();
--- JS Front End/05.HTTP and REST/Exercises/05.Messenger/app.js
+++ JS Front End/05.HTTP and REST/Exercises/05.Messenger/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    // TODO:
+    const textArea = document.getElementById('messages');
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const authorInput = document.getElementById('author');
+    const contentInput = document.getElementById('content');
+    const URL = 'http://localhost:3030/jsonstore/messenger';
+
+    sendBtn.addEventListener('click', sendHandler);
+    refreshBtn.addEventListener('click', refreshHandler);
+
+    async function refreshHandler() {
+        const response = await fetch(URL);
+        const data = await response.json();
+        const messages = [];
+        textArea.innerHTML = '';
+
+        Object.values(data).forEach((msg) => {
+            const {author, content} = msg;
+            messages.push(`${author}: ${content}`);
+        });
+        textArea.textContent = messages.join('\n');
+        authorInput.value = '';
+        contentInput.value = '';
+    }
+
+    async function sendHandler() {
+        const author = authorInput.value;
+        const content = contentInput.value;
+        const httpHeaders = {
+            method: 'POST',
+            body: JSON.stringify({author, content})
+        };
+
+        await fetch(URL, httpHeaders);
+    }
 }
 
 attachEvents();
