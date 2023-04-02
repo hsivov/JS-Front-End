baa04d30306c2cbf1afd4f967643b2704d5e6b86
hsivov <hsivov@students.softuni.bg>
2023-04-02 21:27:06 +0300

    HTTP and REST more exercises

--- JS Front End/05.HTTP and REST/More/04. Furniture/app.js
+++ JS Front End/05.HTTP and REST/More/04. Furniture/app.js
@@ -29,11 +29,66 @@ async function createFurniture(event) {
         await fetch(url, {
             method: 'post',
             headers: {
-                'Content-Type': "application/json"
+                'Content-Type': "application/json",
+                'X-Authorization': token
             },
             body: JSON.stringify(payload)
         });
     } catch (err) {
         alert(err.message);
     }
+
+    createNewRow(name, price, factor, img);
+}
+
+function createNewRow(name, price, factor, img) {
+    const tbody = document.querySelector('tbody');
+    const tableRow = createElement('tr', '', tbody);
+
+    const firstColumn = createElement('td', '', tableRow);
+    createElement('img', '', firstColumn, '', '', {src: img});
+
+    const secondColumn = createElement('td', '', tableRow);
+    createElement('p', name, secondColumn);
+
+    const thirdColumn = createElement('td', '', tableRow);
+    createElement('p', price, thirdColumn);
+
+    const fourthColumn = createElement('td', '', tableRow);
+    createElement('p', factor, fourthColumn);
+
+    const fifthColumn = createElement('td', '', tableRow);
+    createElement('input', '', fifthColumn, '', '', {type: "checkbox"});
+}
+
+function createElement(type, content, parentNode, id, classes, attributes) {
+    const htmlElement = document.createElement(type);
+
+    if (content && type !== 'input') {
+        htmlElement.textContent = content;
+    }
+
+    if (content && type === 'input') {
+        htmlElement.value = content;
+    }
+
+    if (parentNode) {
+        parentNode.appendChild(htmlElement);
+    }
+
+    if (id) {
+        htmlElement.setAttribute('id', id);
+    }
+
+    if (classes) {
+        htmlElement.classList.add(...classes);
+    }
+
+    if (attributes) {
+        for (const key in attributes) {
+            htmlElement.setAttribute(key, attributes[key]);
+        }
+    }
+
+    return htmlElement;
 }
